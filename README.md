0552ae44a38488d299fa207a185a392c20d2c33a
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2023-12-14 05:13:08 -0300

    autoupdate

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ My dotfiles for neovim is started with [kickstart.nvim](https://github.com/nvim-
 
 - [ ] Separate each plugin to its own config file
     - https://www.youtube.com/watch?v=LCUCN69vw_Q (try to mimic this file structure on last chapter)
+
+- [ ] Read all telescope [pickers](https://github.com/nvim-telescope/telescope.nvim#pickers) and see what am I missing 
--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
---asdasd
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.loader.enable()
@@ -281,7 +280,8 @@ vim.keymap.set('n', '<leader>b', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>g', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+-- vim.keymap.set('n', '<leader>g', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = 'Live Grep' })
 vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 --vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>n', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
