f1a6cc0a0a6cdb769815791e0ded74cf7a00f497
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-01-13 07:55:28 -0300

    autoupdate

--- init.lua
+++ init.lua
@@ -33,6 +33,22 @@ require('lazy').setup({
     end,
     opts = {}
   },
+  -- lazy.nvim
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+      }
+  },
   -- Plugin for surrounding text with () {} "", and more
   'tpope/vim-surround',
   -- Detect tabstop and shiftwidth automatically
--- lua/remap.lua
+++ lua/remap.lua
@@ -1,6 +1,6 @@
 --  Move a selected block up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
 --  B to enter visual block mode
 vim.keymap.set("n", "B", "<C-v>")
@@ -15,8 +15,8 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- delete without yanking
-vim.keymap.set("n", "<leader>d", "\"_d", { desc = 'Delete without yanking' })
-vim.keymap.set("v", "<leader>d", "\"_d", { desc = 'Delete without yanking' })
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = 'Delete without yanking', silent = true })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = 'Delete without yanking', silent = true })
 
 -- Remove recording shit
 vim.keymap.set("n", "Q", "<nop>")
@@ -30,20 +30,20 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 -- Terminal
-vim.keymap.set("n", "<leader>t", ":ToggleTerm dir=pwd direction=float <CR>", { desc = 'Open Floating Terminal' })
-vim.keymap.set("n", "<leader>v", ":ToggleTerm size=100 dir=pwd direction=vertical <CR>", { desc = 'Open vertical Terminal' })
+vim.keymap.set("n", "<leader>t", ":ToggleTerm dir=pwd direction=float <CR>", { desc = 'Open Floating Terminal', silent = true })
+vim.keymap.set("n", "<leader>v", ":ToggleTerm size=100 dir=pwd direction=vertical <CR>", { desc = 'Open vertical Terminal', silent = true })
 -- Exit terminal Mode
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
-vim.keymap.set('n', "<leader>c", ":lua vim.lsp.buf.code_action()<CR>", { desc = 'Code Action' })
+vim.keymap.set('n', "<leader>c", ":lua vim.lsp.buf.code_action()<CR>", { desc = 'Code Action', silent = true })
 
 -- nvimtree
-vim.keymap.set('n', "<leader>e", ":NvimTreeToggle <CR>", { desc = 'Toggle Nvim-Tree' })
+vim.keymap.set('n', "<leader>e", ":NvimTreeToggle <CR>", { desc = 'Toggle Nvim-Tree', silent = true })
 -- Spliting remap
-vim.keymap.set('n', "<leader>h", ":wincmd h<CR>", { desc = 'Move to left' })
-vim.keymap.set('n', "<leader>w", ":wincmd w<CR>", { desc = 'Move to next' })
-vim.keymap.set('n', "<leader>l", ":wincmd l<CR>", { desc = 'Move to right' })
-vim.keymap.set('n', "<leader>a", ":vsplit <CR>", { desc = 'Split' })
-vim.keymap.set('n', "<leader>q", ":wincmd c<CR>", { desc = 'Close Buffer' })
+vim.keymap.set('n', "<leader>h", ":wincmd h<CR>", { desc = 'Move to left', silent = true})
+vim.keymap.set('n', "<leader>w", ":wincmd w<CR>", { desc = 'Move to next', silent = true })
+vim.keymap.set('n', "<leader>l", ":wincmd l<CR>", { desc = 'Move to right', silent = true })
+vim.keymap.set('n', "<leader>a", ":vsplit <CR>", { desc = 'Split' , silent = true})
+vim.keymap.set('n', "<leader>q", ":wincmd c<CR>", { desc = 'Close Buffer', silent = true })
 vim.keymap.set('n', "<leader>=", ":wincmd +<CR>")
 vim.keymap.set('n', "<leader>-", ":wincmd -<CR>")
 
