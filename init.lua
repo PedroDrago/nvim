44818a98add8bd91e1e5afee640d3d95f9e74d97
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-01-22 01:24:29 -0300

    autoupdate

--- init.lua
+++ init.lua
@@ -1,32 +1,13 @@
-vim.loader.enable()
--- Including other config files
-require("set")     -- Vim settings
-require("keymaps") -- My keymaps
-
--- Lazy bootstrap
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Load all plugins
-require("lazy").setup("plugins")
+vim.loader.enable()              --Speedup Startup time
+require("set")                   -- Vim settings
+require("keymaps")               -- My keymaps
+require("lazy-init")             -- Lazy bootstrap
+require("lazy").setup("plugins") -- Load all plugins
 
 -- Post import configurations (i have not figured out where to put these witouth breaking)
 require("telescope").load_extension("ui-select")
 require('neodev').setup()
-
 require("lsp-config")
-
-----------------------------
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,16 +27,16 @@ return {
       -- Enable telescope fzf native, if installed
       pcall(require('telescope').load_extension, 'fzf')
       vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-      vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+      -- vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = 'Find existing buffers' })
       vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = 'Live Grep' })
-      vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>n', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search Files' })
+      vim.keymap.set('n', '<leader>n', require('telescope.builtin').grep_string, { desc = 'Search current Word' })
       vim.keymap.set('n', '<leader>b', function()
         require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
+          winblend = 20,
           previewer = false,
         })
-      end, { desc = '[/] Fuzzily search in current buffer' })
+      end, { desc = 'Fuzzily search in current buffer' })
     end,
   },
 }
