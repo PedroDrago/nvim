3e64aa304d9b71aa7e4969204dc3df4ef002c38b
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-02-26 02:33:17 -0300

    new

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,13 +4,14 @@ return { -- Autoformat
     notify_on_error = false,
     format_on_save = {
       timeout_ms = 500,
-      lsp_fallback = true,
+      lsp_fallback = false, --disabled just because of 42 norm for C
     },
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform will run multiple formatters sequentially
       go = { 'goimports', 'gofmt' },
       python = { 'black' },
+      c = {},
       javascript = { { 'prettierd', 'prettier' } },
     },
   },
