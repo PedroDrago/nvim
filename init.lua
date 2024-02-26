5ba89fa8cd271f388fb55651cbfa5b47643a69a8
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-02-26 02:22:59 -0300

    idk

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
-require 'set'
-require 'keymaps'
+require("set")
+require("keymaps")
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup 'plugins'
+require("lazy").setup("plugins")
 
-vim.cmd.colorscheme 'tokyonight-night'
+vim.cmd.colorscheme("tokyonight-night")
 -- vim.cmd.colorscheme 'onedark'
 
 -- vim: ts=2 sts=2 sw=2 et
