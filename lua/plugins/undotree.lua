d33b4adcb0e759cfb72dc34ff04871ad144c922e
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-02-26 04:05:08 -0300

    better undotree plugin

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -6,8 +6,3 @@ return {
     { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
   },
 }
--- return {
---   'mbbill/undotree',
---   vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { silent = true, desc = 'Undo Tree' }),
---   vim.keymap.set('n', '<leader>U', '<cmd>Telescope undo<cr>', { silent = true, desc = 'Undo Tree Telescope' }),
--- }
