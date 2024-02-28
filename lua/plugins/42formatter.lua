cc9c4fc0cd32dcbc83f742ed63b07bf836f8126c
pdrago <jupurity6@gmail.com>
2024-02-28 20:21:08 -0300

    change deprecated function

--- lua/plugins/42formatter.lua
+++ lua/plugins/42formatter.lua
@@ -1,13 +1,13 @@
 return {
   'cacharle/c_formatter_42.vim',
   -- Define autocmd to run :CFormatter42 on BufWritePost for C files
-  vim.api.nvim_exec(
+  vim.api.nvim_exec2(
     [[
   augroup FormatCOnSave
     autocmd!
     autocmd BufWritePost *.c,*.h :CFormatter42
   augroup END
 ]],
-    false
+    {}
   ),
 }
