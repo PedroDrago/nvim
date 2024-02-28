09069b13f49cabb9549356194f50ff7b6d718ac7
pdrago <jupurity6@gmail.com>
2024-02-28 19:51:10 -0300

    42 norm plugin

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd 'colorscheme tokyonight-night'
-vim.g.theme_id = 1
+vim.cmd("colorscheme tokyonight-storm")
+vim.g.theme_id = 2
 -- end themery block
