bcfbf8e4c35d2f44dd55a299b1ed2b681ba68e6e
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-02-28 13:23:32 -0300

    Trouble Toggle

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -5,7 +5,17 @@ return {
   opts = {
     mode = 'workspace_diagnostics',
   },
-  vim.keymap.set('n', '<leader>z', function()
-    require('trouble').toggle()
-  end, { silent = true, desc = 'Trouble Project' }),
+  config = function()
+    local troubleEnable = false
+    vim.api.nvim_create_user_command('ToggleTroubleWorkspace', function()
+      if troubleEnable then
+        troubleEnable = false
+        vim.api.nvim_command 'TroubleClose'
+      else
+        troubleEnable = true
+        vim.api.nvim_command 'Trouble workspace_diagnostics'
+      end
+    end, {})
+  end,
+  vim.keymap.set('n', '<leader>z', ':ToggleTroubleWorkspace<CR>', { silent = true, desc = 'Trouble Project' }),
 }
