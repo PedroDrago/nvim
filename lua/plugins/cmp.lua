cac81db2ebc8f9ae9ad0e76f16fcf47f0fa4971e
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-01 04:11:37 -0300

    new cmp keymaps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -59,7 +59,7 @@ return {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p'] = cmp.mapping.select_prev_item(),
         -- ['<CR>'] = cmp.mapping.confirm { select = false },
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<C-Enter>'] = cmp.mapping.confirm { select = true },
         ['<C-Space>'] = cmp.mapping.complete {},
         ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
