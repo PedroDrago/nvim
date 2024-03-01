0f0c20f21dd0ad46ad56dfb575eace0fa00cdc88
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-02-29 21:34:03 -0300

    create norm command

--- lua/plugins/42formatter.lua
+++ lua/plugins/42formatter.lua
@@ -1,12 +1,4 @@
 return {
   'cacharle/c_formatter_42.vim',
-  vim.api.nvim_exec2(
-    [[
-  augroup FormatCOnSave
-    autocmd!
-    autocmd BufWritePost *.c,*.h :CFormatter42
-  augroup END
-]],
-    {}
-  ),
+  vim.api.nvim_create_user_command('Norm', ':CFormatter42', {}),
 }
