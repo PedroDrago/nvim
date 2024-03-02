b8e8149010eb86d1dbe72dfab1eda935ffd0ba6b
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-01 21:32:00 -0300

    better mappings telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,16 @@ return {
     { 'nvim-tree/nvim-web-devicons' },
   },
   config = function()
+    local actions = require 'telescope.actions'
     require('telescope').setup {
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-j>'] = actions.move_selection_next,
+            ['<C-k>'] = actions.move_selection_previous,
+          },
+        },
+      },
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
