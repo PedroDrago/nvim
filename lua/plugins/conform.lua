b939b0b2fdd41264a15e691f330a86884e556df7
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-19 05:31:26 -0300

    readme

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,15 +3,17 @@ return {
   event = 'VeryLazy',
   opts = {
     notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = false, --disabled just because of 42 norm for C
-    },
+    format_on_save = function(bufnr)
+      local disable_filetypes = { c = true, cpp = true }
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
+    end,
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'gofmt', 'goimports' }, --this is quite slow, may be disable for formating on save
       python = { 'black' },
-      c = {},
       sql = { 'sql-formatter' },
       javascript = { { 'prettierd', 'prettier' } },
       ruby = { 'rubocop' },
