e832c29e8d18ec1168a3358bf181626b63525eed
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-19 02:31:37 -0300

    remove FUCKING ANOYING noice signature shit

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,23 +1,48 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {
-    lsp = {
-      override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
-        ['cmp.entry.get_documentation'] = true,
-      },
-    },
-    presets = {
-      bottom_search = false,
-      command_palette = false,
-      long_message_to_split = true,
-      inc_rename = false,
-      lsp_doc_border = true,
-    },
-  },
   dependencies = {
     'MunifTanjim/nui.nvim',
   },
+  config = function()
+    require('noice').setup {
+      cmdline = {
+        enabled = true,
+        view = 'cmdline_popup',
+        opts = {},
+        format = {
+          cmdline = { pattern = '^:', icon = '', lang = 'vim' },
+          search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+          search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+          filter = { pattern = '^:%s*!', icon = '$', lang = 'bash' },
+          lua = { pattern = { '^:%s*lua%s+', '^:%s*lua%s*=%s*', '^:%s*=%s*' }, icon = '', lang = 'lua' },
+          help = { pattern = '^:%s*he?l?p?%s+', icon = '' },
+          input = {},
+        },
+      },
+      presets = {
+        bottom_search = false,
+        command_palette = false,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = true,
+      },
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+        hover = {
+          enabled = true,
+          silent = false,
+          view = nil,
+          opts = {},
+        },
+        signature = {
+          enabled = false,
+        },
+      },
+    }
+  end,
 }
