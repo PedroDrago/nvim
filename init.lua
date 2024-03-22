7f83e3933467a32e044c58683267d635efff6f34
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-21 22:23:06 -0300

    theme revamp

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 require 'set'
 require 'keymaps'
 require 'lazyvim'
-require 'colors'
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+      },
+    },
+    init = function()
+      vim.cmd 'colorscheme tokyonight-night'
+    end,
+  },
+}
