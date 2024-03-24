9c0007f0f4222661d50e9186487d376660d76c55
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-24 18:52:54 -0300

    more buttons to alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -25,13 +25,16 @@ return {
       '                                                     ',
     }
     dashboard.section.buttons.val = {
-      dashboard.button('d', '  > Dotfiles', ':e $MYVIMRC | cd %:h<CR>'),
+      dashboard.button('d', '  > Config', ':e $MYVIMRC | cd %:h<CR>'),
+      dashboard.button('r', '  > Recent', ":lua require'telescope.builtin'.oldfiles()<CR>"),
       dashboard.button('l', '󰒲  > Lazy', ':Lazy<CR>'),
+      dashboard.button('s', '  > Startup', ':Lazy profile<CR>'),
       dashboard.button('h', '✚  > Health', ':checkhealth<CR>'),
       dashboard.button('q', '󰗼  > Quit', ':q!<CR>'),
     }
     dashboard.section.header.opts.hl = 'Function'
     dashboard.section.footer.val = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'Drago' }
+    --  󰈞
 
     vim.cmd [[
         autocmd FileType alpha setlocal nofoldenable
