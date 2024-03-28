a302f21d35d58bd827a2b634f5dcca1549394c64
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-27 22:20:25 -0300

    more minimalistic approach

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -32,12 +32,10 @@ return {
       dashboard.button('d', '  > Config', ':e $MYVIMRC | cd %:h<CR>'),
       dashboard.button('r', '  > Recent', ":lua require'telescope.builtin'.oldfiles()<CR>"),
       dashboard.button('l', '󰒲  > Lazy', ':Lazy<CR>'),
-      dashboard.button('s', '  > Startup', ':Lazy profile<CR>'),
-      dashboard.button('h', '✚  > Health', ':checkhealth<CR>'),
       dashboard.button('q', '󰗼  > Quit', ':q!<CR>'),
     }
     dashboard.section.header.opts.hl = 'Function'
-    dashboard.section.footer.val = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'Drago' }
+    dashboard.section.footer.val = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'Drago' }
 
     vim.cmd [[
         autocmd FileType alpha setlocal nofoldenable
