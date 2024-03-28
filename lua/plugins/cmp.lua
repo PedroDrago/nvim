217278d5675b06cb561fbfef00d61d9c079b88a0
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-27 22:20:11 -0300

    redundant keymaps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -50,9 +50,10 @@ return {
       mapping = cmp.mapping.preset.insert {
         ['<C-j>'] = cmp.mapping.select_next_item(),
         ['<C-k>'] = cmp.mapping.select_prev_item(),
-        ['<C-Enter>'] = cmp.mapping.confirm { select = true }, --NOTE: Multiple mappings for multiple terminals, systems etc
-        ['<C-CR>'] = cmp.mapping.confirm { select = true }, --NOTE:    Multiple mappings for multiple terminals, systems etc
-        ['<C-M>'] = cmp.mapping.confirm { select = true }, --NOTE:     Multiple mappings for multiple terminals, systems etc
+        ['<C-Enter>'] = cmp.mapping.confirm { select = true }, --      NOTE: Multiple mappings for multiple terminals, systems etc
+        ['<C-CR>'] = cmp.mapping.confirm { select = true }, --         NOTE: Multiple mappings for multiple terminals, systems etc
+        ['<C-M>'] = cmp.mapping.confirm { select = true }, --          NOTE: Multiple mappings for multiple terminals, systems etc
+        ['<C-Return>'] = cmp.mapping.confirm { select = true }, --     NOTE: Multiple mappings for multiple terminals, systems etc
         ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
@@ -72,4 +73,4 @@ return {
     }
   end,
 }
--- NOTE: apparently coq is very faster than cmp
+-- NOTE: apparently coq is much faster than cmp
