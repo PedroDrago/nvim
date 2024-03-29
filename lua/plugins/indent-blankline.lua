89453606fbe217edce52b8ad10c9f02d439e4f22
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-29 20:59:50 -0300

    fix plugin: indent blankline was using default config instead of custom

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,38 +4,30 @@ return {
     {
       '<leader>i',
       function()
-        require('ibl').setup {
-          require('ibl').setup_buffer(0, {
-            enabled = not require('ibl.config').get_config(0).enabled,
-          }),
-        }
+        require('ibl').setup_buffer(0, {
+          enabled = not require('ibl.config').get_config(0).enabled,
+        })
       end,
     },
   },
-  config = function()
-    require('ibl').setup {
-      enabled = false,
-      indent = {
-        char = '│',
-        tab_char = '│',
+  opts = {
+    enabled = false,
+    indent = {
+      char = '│',
+      tab_char = '│',
+    },
+    scope = { enabled = false },
+    exclude = {
+      filetypes = {
+        'help',
+        'alpha',
+        'dashboard',
+        'lazy',
+        'mason',
+        'terminal',
+        'spectre',
       },
-      scope = { enabled = false },
-      exclude = {
-        filetypes = {
-          'help',
-          'alpha',
-          'dashboard',
-          'neo-tree',
-          'Trouble',
-          'trouble',
-          'lazy',
-          'mason',
-          'notify',
-          'toggleterm',
-          'lazyterm',
-        },
-      },
-    }
-  end,
+    },
+  },
   main = 'ibl',
 }
