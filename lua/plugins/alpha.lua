989cd5aedf81b4404a76dfd83b7d565a6d4e5341
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-29 18:06:49 -0300

    config plugin: alpha event and 2 opts

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,5 +1,8 @@
 return {
   'goolord/alpha-nvim',
+  event = 'VimEnter',
+  enabled = true,
+  init = false,
   config = function()
     require('alpha').setup(require('alpha.themes.dashboard').config)
     local dashboard = require 'alpha.themes.dashboard'
