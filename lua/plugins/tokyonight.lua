24ddb8ca02ac8be4448468524a87fb599e2fb384
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-29 11:58:02 -0300

    theme hl

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -5,21 +5,44 @@ return {
     priority = 1000,
     opts = {
       styles = {
-        comments = { italic = false },
+        comments = { italic = false }, -- fg | bg | sp | blend | | bold | standout | underline | undercurl | underdouble | underdoted | underdashed | strikethrough | italic | reverse | nocombine | link | default | ctermfg | ctermbg | cterm | set | map | force
         keywords = { italic = false },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = 'dark',
+        floats = 'dark',
       },
       sidebars = { 'help' },
       day_brightness = 0.5,
       hide_inactive_statusline = false,
       dim_inactive = false,
       lualine_bold = false,
+      -----------------------------------
+      ---@param colors ColorScheme
+      on_colors = function(colors) -- Change colors
+        -- colors.hint = colors.orange
+        -- colors.error = '#ff0000'
+      end,
+      ---@param hl Highlights
+      ---@param c ColorScheme
+      on_highlights = function(hl, c) -- Change highlights
+        hl.MiniDiffSignAdd = {
+          bg = '', -- c.bg_dark
+          fg = '', -- c.fg_drak
+        }
+        hl.MiniDiffSignDelete = {
+          bg = '',
+          fg = '',
+        }
+        hl.MiniDiffSignChange = {
+          bg = '',
+          fg = '',
+        }
+      end,
     },
     init = function()
       vim.cmd 'colorscheme tokyonight-night'
-      -- Remove tokyonight highlight for minidiff git signs in signcolumn
-      vim.api.nvim_set_hl(0, 'MiniDiffSignAdd', { fg = '' })
-      vim.api.nvim_set_hl(0, 'MiniDiffSignDelete', { fg = '' })
-      vim.api.nvim_set_hl(0, 'MiniDiffSignChange', { fg = '' })
     end,
   },
 }
