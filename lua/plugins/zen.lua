ddaab5d0a77e001ab43b19b713ec24552fc8eeb4
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-30 12:19:28 -0300

    remove twilight

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -1,46 +1,39 @@
 return {
-  'folke/zen-mode.nvim',
-  dependencies = {
-    {
-      'folke/twilight.nvim',
-      opts = {},
-    },
-  },
-  keys = { { '<leader>z', ':ZenMode<CR>', desc = 'Zen Mode', silent = true } },
-  opts = {
-    window = {
-      backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-      width = 120, -- width of the Zen window
-      height = 1, -- height of the Zen window
-      options = {
-        signcolumn = 'no', -- disable signcolumn
-        number = false, -- disable number column
-        relativenumber = false, -- disable relative numbers
-        foldcolumn = '0', -- disable fold column
+  {
+    'folke/zen-mode.nvim',
+    dependencies = {},
+    keys = { { '<leader>z', ':ZenMode<CR>', desc = 'Zen Mode', silent = true } },
+    opts = {
+      window = {
+        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+        width = 120, -- width of the Zen window
+        height = 1, -- height of the Zen window
+        options = {
+          signcolumn = 'no', -- disable signcolumn
+          number = false, -- disable number column
+          relativenumber = false, -- disable relative numbers
+          foldcolumn = '0', -- disable fold column
+        },
       },
-    },
-    plugins = {
-      options = {
-        enabled = true,
-        ruler = false,
-        showcmd = false,
-        laststatus = 0, -- turn off the statusline in zen mode
+      plugins = {
+        options = {
+          enabled = true,
+          ruler = false,
+          showcmd = false,
+          laststatus = 0, -- turn off the statusline in zen mode
+        },
+        twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
       },
-      twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
-      alacritty = {
-        enabled = false,
-        font = '14', -- font size
-      },
+      -- callback where you can add custom code when the Zen window opens
+      on_open = function(win)
+        vim.diagnostic.disable()
+        require('cmp').setup.buffer { enabled = false }
+      end,
+      -- callback where you can add custom code when the Zen window closes
+      on_close = function()
+        vim.diagnostic.enable()
+        require('cmp').setup.buffer { enabled = true }
+      end,
     },
-    -- callback where you can add custom code when the Zen window opens
-    on_open = function(win)
-      vim.diagnostic.disable()
-      require('cmp').setup.buffer { enabled = false }
-    end,
-    -- callback where you can add custom code when the Zen window closes
-    on_close = function()
-      vim.diagnostic.enable()
-      require('cmp').setup.buffer { enabled = true }
-    end,
   },
 }
