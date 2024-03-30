2521a87079798d51eef0bb5189361d79d91f725e
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-30 13:35:36 -0300

    idk

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -1,23 +1,43 @@
 return {
-  'folke/zen-mode.nvim',
-  dependencies = {},
-  keys = { { '<leader>z', ':ZenMode<CR>', desc = 'Zen Mode', silent = true } },
-  opts = {
-    window = {
-      options = {
-        signcolumn = 'no',
-        number = false,
-        relativenumber = false,
-        foldcolumn = '0',
+  {
+    'folke/zen-mode.nvim',
+    dependencies = {},
+    keys = { { '<leader>z', ':ZenMode<CR>', desc = 'Zen Mode', silent = true } },
+    opts = {
+      plugins = {
+        twilight = { enabled = false },
       },
+      window = {
+        options = {
+          signcolumn = 'no',
+          number = false,
+          relativenumber = false,
+          foldcolumn = '0',
+        },
+      },
+      on_open = function(win)
+        vim.api.nvim_command 'LspStop'
+        require('cmp').setup.buffer { enabled = false }
+      end,
+      on_close = function()
+        vim.api.nvim_command 'LspStart'
+        vim.api.nvim_command 'TwilightDisable'
+        require('cmp').setup.buffer { enabled = true }
+      end,
     },
-    on_open = function(win)
-      vim.api.nvim_command 'LspStop'
-      require('cmp').setup.buffer { enabled = false }
-    end,
-    on_close = function()
-      vim.api.nvim_command 'LspStart'
-      require('cmp').setup.buffer { enabled = true }
-    end,
+  },
+  {
+    'folke/twilight.nvim',
+    cmd = 'Twilight',
+    opts = {
+      expand = {
+        'function_definition',
+        -- 'function',
+        -- 'method',
+        -- 'table',
+        -- 'if_statement',
+      },
+      exclude = {}, -- exclude these filetypes
+    },
   },
 }
