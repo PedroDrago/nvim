c5d4a122765ab9dd391979b572292d2cba85b510
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-03-31 11:39:21 -0300

    plugin add/config: change to original undotree and configure

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,13 +1,13 @@
 return {
-  'jiaoshijie/undotree',
-  dependencies = 'nvim-lua/plenary.nvim',
-  config = true,
-  opts = {
-    window = {
-      winblend = 0,
-    },
-  },
+  'mbbill/undotree',
   keys = {
-    { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", desc = 'Undo Tree', silent = true },
+    { '<leader>u', ':UndotreeToggle<CR>', desc = 'Undo Tree', silent = true },
   },
+  config = function()
+    vim.g.undotree_WindowLayout = 2
+    vim.g.undotree_SplitWidth = 40
+    vim.g.undotree_DiffpanelHeight = 15
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_DiffCommand = 'diff --color'
+  end,
 }
