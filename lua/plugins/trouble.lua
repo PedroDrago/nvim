ae18739998ff0a1724c1ee4c686278a069372d28
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-04-10 11:00:28 -0300

    new plugin: trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,11 @@
+return {
+  'folke/trouble.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    { '<leader>x', ':TroubleToggle<CR>', desc = 'Toggle Troubble', silent = true },
+  },
+  opts = {
+    auto_fold = true,
+    use_diagnostic_signs = true,
+  },
+}
