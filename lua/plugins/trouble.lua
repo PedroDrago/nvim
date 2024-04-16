767d907caeda16f9dc22d2d6d488ac407b064865
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-04-16 08:18:30 -0300

    plugin config: change trouble keymap to be only to document diagnostics

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,7 +2,7 @@ return {
   'folke/trouble.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   keys = {
-    { '<leader>x', '<cmd>TroubleToggle<CR>', desc = 'Toggle Troubble' },
+    { '<leader>x', '<cmd>TroubleToggle document_diagnostics<CR>', desc = 'Toggle Troubble' },
   },
   opts = {
     auto_fold = true,
