1ed24b96e71321567d95aa6a95f935e90a323b18
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-04-20 10:20:52 -0300

    format and config and add type definition keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,6 +9,7 @@ return {
     { 'j-hui/fidget.nvim', opts = { progress = { display = { done_ttl = 7 } } } },
   },
   config = function()
+    -- SECTION: Diagnostic Toggle Function
     local diagnostics_enabled = true
     vim.api.nvim_create_user_command('DiagnosticsToggle', function()
       if diagnostics_enabled then
@@ -21,13 +22,13 @@ return {
         vim.cmd 'echo "Diagnostics On"'
       end
     end, { desc = 'Toggle LSP Diagnostics On/Off' })
-    -- vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {})
+    -- SECTION: Diagnostics Config
     vim.diagnostic.config { -- :h vim.diagnostic.config
       underline = true,
       update_in_insert = false,
       virtual_text = {
         spacing = 4,
-        source = false, --use 'if_many' when linting
+        source = false, -- TODO: change this to a function -> if global variable linting then 'if_many' else false
         prefix = function(diagnostic)
           if diagnostic.severity == vim.diagnostic.severity.ERROR then
             return ' '
@@ -35,7 +36,7 @@ return {
             return ' '
           elseif diagnostic.severity == vim.diagnostic.severity.INFO then
             return ' '
-          else -- diagnostic.severity == vim.diagnostic.severity.HINT then
+          else -- HINT
             return ' '
           end
         end,
@@ -45,10 +46,10 @@ return {
       codelens = { enabled = true },
       signs = {
         text = {
-          [vim.diagnostic.severity.ERROR] = '', --'',
-          [vim.diagnostic.severity.WARN] = '', --'',
-          [vim.diagnostic.severity.HINT] = '', --'',
-          [vim.diagnostic.severity.INFO] = '', --'',
+          [vim.diagnostic.severity.ERROR] = '',
+          [vim.diagnostic.severity.WARN] = '',
+          [vim.diagnostic.severity.HINT] = '',
+          [vim.diagnostic.severity.INFO] = '',
         },
         numhl = {
           [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
@@ -58,6 +59,7 @@ return {
         },
       },
     }
+    -- SECTION: General LSP Tools on attach
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -75,9 +77,10 @@ return {
         map('gS', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
         map('<leader>s', vim.lsp.buf.rename, 'Rename')
         vmap('<leader>c', vim.lsp.buf.code_action, 'Code Action')
+        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
         map('K', vim.lsp.buf.hover, 'Hover Documentation')
         map('J', vim.diagnostic.open_float, 'Expand Diagnostic')
-        map('L', vim.lsp.buf.signature_help, 'Signature Documentation')
+        map('S', vim.lsp.buf.signature_help, 'Signature Documentation') --maybe H is better
         map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
@@ -94,6 +97,7 @@ return {
         end
       end,
     })
+    -- SECTION: Language Servers Individual Configurations
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
     local servers = {
@@ -126,6 +130,7 @@ return {
         },
       },
     }
+    -- SECTION: Mason
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua',
