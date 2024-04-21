6ed2a56d24d9d3b782f5c1ad668016ca8ea8c071
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-04-21 10:12:06 -0300

    fix: oil float bug

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,54 +1,53 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    config = function()
-      require('oil').setup {
+  'stevearc/oil.nvim',
+  tag = 'v2.8.0', -- BUG: Remove when [Issue](https://github.com/stevearc/oil.nvim/issues/355) has been resolved.
+  config = function()
+    require('oil').setup {
+      win_options = {
+        wrap = false,
+        cursorcolumn = false,
+        foldcolumn = '0',
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = 'nvic',
+      },
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = 'actions.select_vsplit',
+        ['<C-h>'] = '<C-w><C-h>',
+        ['<C-l>'] = '<C-w><C-l>',
+        ['<C-t>'] = 'actions.select_tab',
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['-'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = 'actions.tcd',
+        ['gs'] = 'actions.change_sort',
+        ['gx'] = 'actions.open_external',
+        ['g.'] = 'actions.toggle_hidden',
+        ['g\\'] = 'actions.toggle_trash',
+      },
+      float = {
+        padding = 2,
+        max_width = 80,
+        max_height = 35,
+        border = 'rounded',
         win_options = {
-          wrap = false,
-          cursorcolumn = false,
-          foldcolumn = '0',
-          spell = false,
-          list = false,
-          conceallevel = 3,
-          concealcursor = 'nvic',
+          winblend = 0,
         },
-        keymaps = {
-          ['g?'] = 'actions.show_help',
-          ['<CR>'] = 'actions.select',
-          ['<C-s>'] = 'actions.select_vsplit',
-          ['<C-h>'] = '<C-w><C-h>',
-          ['<C-l>'] = '<C-w><C-l>',
-          ['<C-t>'] = 'actions.select_tab',
-          ['<C-p>'] = 'actions.preview',
-          ['<C-c>'] = 'actions.close',
-          ['-'] = 'actions.parent',
-          ['_'] = 'actions.open_cwd',
-          ['`'] = 'actions.cd',
-          ['~'] = 'actions.tcd',
-          ['gs'] = 'actions.change_sort',
-          ['gx'] = 'actions.open_external',
-          ['g.'] = 'actions.toggle_hidden',
-          ['g\\'] = 'actions.toggle_trash',
-        },
-        float = {
-          padding = 2,
-          max_width = 80,
-          max_height = 35,
-          border = 'rounded',
-          win_options = {
-            winblend = 0,
-          },
-        },
-        skip_confirm_for_simple_edits = true,
-        view_options = {
-          show_hidden = true,
-        },
-      }
-    end,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    vim.keymap.set('n', '<leader>e', function()
-      require('oil').toggle_float()
-    end, { desc = 'Oil Float' }),
-    vim.keymap.set('n', '<leader>E', '<CMD>Oil<CR>', { desc = 'Oil Buffer' }),
-  },
+      },
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
+    }
+  end,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  vim.keymap.set('n', '<leader>e', function()
+    require('oil').toggle_float()
+  end, { desc = 'Oil Float' }),
+  vim.keymap.set('n', '<leader>E', '<CMD>Oil<CR>', { desc = 'Oil Buffer' }),
 }
