b91d45548de7c8823d29a38997cd9bd69a4391e7
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-05-22 09:32:20 -0300

    Test

--- init.lua
+++ init.lua
@@ -6,4 +6,8 @@ require 'autocmds'
 require 'colorscheme'
 require 'lazyvim'
 
+vim.keymap.set('n', '<leader>0', function ()
+  vim.g.tree = not vim.g.tree
+end, { desc = 'Description', silent = true })
+
 -- vim: ts=2 sts=2 sw=2
