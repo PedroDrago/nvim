e1acb8f0513241169931c10c315558ff97558842
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-07-13 06:47:18 -0300

    lualine style

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -28,7 +28,7 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
-        lualine_x = { copilot, 'filetype' },
+        lualine_x = { 'filetype', copilot },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
       },
