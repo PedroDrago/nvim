ed425050e7a5773ea6de881b642f60b44bba7034
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-08-13 09:34:24 -0300

    comment

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,7 +1,7 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  -- TODO: Configure this
+  -- TODO: Configure this (make two modes, one where everything is showed and other where only necessary things shows [i.e. thins I don't forget don't show])
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
