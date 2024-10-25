d361775b19dc90b5cc36ef270be66cafcc7a5771
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-10-25 12:07:22 -0300

    update snippets

--- snippets/c.lua
+++ snippets/c.lua
@@ -19,33 +19,6 @@ ls.add_snippets('c', {
 })
 
 ls.add_snippets('c', {
-  snipept('f', {
-    insert(1),
-    text '(',
-    insert(2),
-    text ')',
-    text { '', '{', '\t' },
-    insert(3),
-    text { '', '\treturn (' },
-    insert(4),
-    text ');',
-    text { '', '}' },
-  }),
-})
-
-ls.add_snippets('cpp', {
-  snipept('t', {
-    text 'typedef struct ',
-    insert(1, 'Name'),
-    text { '', '{', '\t' },
-    insert(2),
-    text { '', '} \t' },
-    rep(1),
-    text ';',
-  }),
-})
-
-ls.add_snippets('c', {
   snipept('ifnul', {
     text 'if (!',
     insert(1, 'Expression'),
@@ -55,59 +28,12 @@ ls.add_snippets('c', {
 })
 
 ls.add_snippets('c', {
-  snipept('v', {
-    text '(void) ',
-    insert(1, 'Variable'),
-  }),
-})
-
-ls.add_snippets('c', {
-  snipept('ws', {
-    text 'while(',
-    insert(1, 'Array'),
-    text '[',
-    insert(2, 'Index'),
-    text { '])', '{' },
-    text { '', '\t' },
-    insert(3),
-    text { '', '}' },
-  }),
-})
-
-ls.add_snippets('c', {
-  snipept('w', {
-    text 'while(',
-    insert(1, 'Condition'),
-    text { ')', '{' },
-    text { '', '\t' },
-    insert(2),
-    text { '', '}' },
-  }),
-})
-
-ls.add_snippets('c', {
-  snipept('m', {
+  snipept('malloc', {
     insert(1),
     text ' = ',
     text 'malloc(sizeof(',
     insert(2, 'Type'),
     text ') * ',
-    insert(3, 'Quantity'),
-    text ');',
-    text { '', 'if (!' },
-    rep(1),
-    text { ')', '\treturn (NULL);', '' },
-    insert(4),
-  }),
-})
-
-ls.add_snippets('c', {
-  snipept('c', {
-    insert(1),
-    text ' = ',
-    text 'ft_calloc(sizeof(',
-    insert(2, 'Type'),
-    text '), ',
     insert(3, 'Quantity'),
     text ');',
     text { '', 'if (!' },
--- snippets/go.lua
+++ snippets/go.lua
@@ -17,49 +17,6 @@ ls.add_snippets('go', {
 })
 
 ls.add_snippets('go', {
-  snipept('f', {
-    text 'func ',
-    insert(1, 'Name'),
-    text '(',
-    insert(2, 'Args'),
-    text ') ',
-    insert(3, 'Return'),
-    text ' {',
-    text { '', '\t' },
-    insert(4),
-    text { '', '}' },
-  }),
-})
-
-ls.add_snippets('go', {
-  snipept('method', {
-    text 'func ',
-    text '(',
-    insert(1, 'Receiver'),
-    text ') ',
-    insert(2, 'Name'),
-    text '(',
-    insert(3),
-    text ') ',
-    insert(4),
-    text { ' {', '\t' },
-    insert(5),
-    text { '', '}' },
-  }),
-})
-
-ls.add_snippets('go', {
-  snipept('t', {
-    text 'type ',
-    insert(1, 'Name'),
-    text { ' struct {', '\t' },
-    insert(2),
-    text { '', '}', '' },
-    insert(3),
-  }),
-})
-
-ls.add_snippets('go', {
   snipept('timeout', {
     text 'ctx, cancel := context.WithTimeout(context.Background(), ',
     insert(1, 'Time'),
@@ -80,18 +37,6 @@ ls.add_snippets('go', {
     text { '', 'default: ', '\t' },
     insert(3, ''),
 
-    text { '', '}' },
-  }),
-})
-
-ls.add_snippets('go', {
-  snipept('handler', {
-    text { 'func (' },
-    insert(1, 'receiver'),
-    text { ') ' },
-    insert(2, 'handler'),
-    text { '(writer http.ResponseWriter, req *http.Request) {', '\t' },
-    insert(3, ''),
     text { '', '}' },
   }),
 })
