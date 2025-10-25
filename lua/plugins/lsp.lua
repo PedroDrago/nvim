f956ac1fec9e81692b19e1b58af2d5559649ac10
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2025-10-25 19:01:49 -0300

    fix: update lsp config to 0.11

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<leader>i', function()
-      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = 0 }, { bufnr = 0 })
     end, { desc = 'Toggle Inlay Hints', silent = true })
     vim.lsp.set_log_level 'off' -- INFO: Change this to "debug" when needed
     vim.api.nvim_create_autocmd('LspAttach', {
@@ -39,15 +39,26 @@ return {
         map('n', 'gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
+            group = highlight_augroup,
             callback = vim.lsp.buf.document_highlight,
           })
 
           vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
             buffer = event.buf,
+            group = highlight_augroup,
             callback = vim.lsp.buf.clear_references,
+          })
+
+          vim.api.nvim_create_autocmd('LspDetach', {
+            group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+            end,
           })
         end
       end,
@@ -100,35 +111,39 @@ return {
       'stylua',
       -- 'clangd',
     })
-    local lspconfig = require 'lspconfig'
-    for server, config in pairs(servers) do
-      -- passing config.capabilities to blink.cmp merges with the capabilities in your
-      -- `opts[server].capabilities, if you've defined it
+
+    -- Configure LSP servers using vim.lsp.config (Neovim 0.11+)
+    for server_name, config in pairs(servers) do
       config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-      lspconfig[server].setup(config)
+
+      -- Register the LSP server config
+      vim.lsp.config[server_name] = {
+        cmd = config.cmd,
+        root_dir = config.root_dir,
+        filetypes = config.filetypes,
+        settings = config.settings,
+        capabilities = config.capabilities,
+        on_attach = function(client)
+          -- Disable semantic tokens for performance
+          client.server_capabilities.semanticTokensProvider = nil
+        end,
+      }
     end
+
     require('mason').setup {
       ui = {
         border = 'single',
       },
     }
-    local lsp = require 'lspconfig'
-    lsp.util.default_config = vim.tbl_extend('force', lsp.util.default_config, { -- NOTE: THIS DISABLES SEMANTIC TOKENS, THIS IS SUPOSED TO SPEED UP LSP, BUT MAY BE IS NOT WORTH
-      on_attach = function(client)
-        client.server_capabilities.semanticTokensProvider = nil
-      end,
-    })
+
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
     require('mason-lspconfig').setup {
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
-          require('lspconfig')[server_name].setup {
-            cmd = server.cmd,
-            settings = server.settings,
-            filetypes = server.filetypes,
-            capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {}),
-          }
+
+          -- Enable the LSP server for matching filetypes
+          vim.lsp.enable(server_name)
         end,
       },
     }
